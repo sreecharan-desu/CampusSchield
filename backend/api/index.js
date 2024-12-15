053f0819b3d37b4bb27ef9d0e28a9b65375c4c8a
sreecharan-desu <sreecharan309@gmail.com>
2024-12-15 12:02:30 +0530

    customized

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,9 +3,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/', function (req, res) {
-	res.send("Hi");
+	res.send("Hi Sreecharan");
 });
 
 app.listen(3000, () => console.log('Server ready on port 3000.'));
 
-module.exports = app;
+// module.exports = app;
